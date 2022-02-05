ff062e64666b3e099220c0358995d7c4868b5c61
junlinchen12138 <97692984+junlinchen12138@users.noreply.github.com>
2022-02-04 22:05:41 -0800

    incorrect

--- Skill1.java
+++ Skill1.java
@@ -1,1 +1,5 @@
-
+public class Skill1{
+    public static int twotimes(int n){
+        return 2 * n;
+    }
+}
---
+++ Skill1Tester.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class Skill1Tester {
+    @Test
+    public void addition() {
+        assertEquals(2, Skill1.twotimes(2));
+    }
+    
+}
